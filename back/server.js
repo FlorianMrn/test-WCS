d97f80c447910ea5b683c469df74d61b9c418f67
Florian Merrien <fmerrien.dev@gmail.com>
2020-09-10 12:39:30 +0200

    Routes Create and Read done and tested with PostMan

--- back/server.js
+++ back/server.js
@@ -6,6 +6,35 @@ const db = require('./models/');
 
 app.use(bodyParser.json());
 
+const success = (res, payload) => {
+    return res.status(200).json(payload)
+};
+
+app.get("/argonautes", async (req, res, next) => {
+    try {
+        const argonautes = await db.Argonaute.find({})
+        return success(res, argonautes)
+    } catch (err) {
+        next({ status : 400, message : "failed to get Argonautes"})
+    }
+});
+
+app.post("/argonautes", async (req, res, next) => {
+    try {
+        const argonaute = await db.Argonaute.create(req.body)
+        return success(res, argonaute)
+    } catch (err) {
+        next({ status: 400, message : "Failed to create an Argonaute"})
+    }
+});
+
+app.use( (err, req, res, next) => {
+    return res.status(err.status || 400 ).json({
+        status : err.status || 400,
+        message : err.message || "Error processing request"
+    })
+});
+
 app.listen(PORT, () => {
     console.log(`listenning on ${PORT}`);
 });
